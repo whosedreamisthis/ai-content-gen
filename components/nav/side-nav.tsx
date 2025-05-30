85c483922e2eb97c4adcc141a42b8b1be6c728e4
Dana Sharon <whosedreamisthis@gmail.com>
2025-05-29 19:56:58 -0700

    sidebar responsibe

--- components/nav/side-nav.tsx
+++ components/nav/side-nav.tsx
@@ -37,7 +37,7 @@ export default function SideNav() {
 
 	console.log('path', path);
 	return (
-		<div className="h-screen p-5 shadow-sm border">
+		<div className="h-screen p-3 shadow-sm border">
 			{menu.map((item, index) => (
 				// <div
 				// 	key={index}
@@ -54,12 +54,14 @@ export default function SideNav() {
 						path === item.path
 							? 'bg-primary text-white'
 							: 'hover:bg-primary hover:text-white'
-					} flex m-2 mr-4 p-2 rounded-lg cursor-pointer`}
+					} flex m-2 mr-2 p-2 rounded-lg cursor-pointer`}
 				>
 					<Link href={item.path}>
-						<div className="flex">
-							<item.icon />
-							<span className="ml-2">{item.name}</span>
+						<div className="flex items-center justify-center md:justify-start">
+							<item.icon className="w-6 h-6" />{' '}
+							<span className="ml-2 hidden md:inline">
+								{item.name}
+							</span>
 						</div>
 					</Link>
 				</div>
